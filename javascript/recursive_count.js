c76a9c8765c540e76ec401728237e2d2af17b136
gamzekandara <gamzekandara@gmail.com>
2025-04-22 17:19:39 -0400

    add js solution

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
   // type your code here
+
+  if(num < 10){
+    console.log(num)
+    recursiveCount(num + 1);
+  }
 }
 
 if (require.main === module) {
@@ -10,4 +15,11 @@ module.exports = recursiveCount;
 
 // OPTIONAL
 // Please add your pseudocode to this file
+
+// log the numbers from 0 to 9 with recursive call functions.
+// console log the number => base case if num is less than 10.
+// call function again until it reaches base case.
+
+
+
 // And a written explanation of your solution
